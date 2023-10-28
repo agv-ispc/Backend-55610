9637dc7279e513615b8f81ac294b0ab0e868cfa7
Adrian Gimenez <106855460+agv-ispc@users.noreply.github.com>
2023-10-28 18:15:01 -0300

    modified:   entrega-01/ProductManager.js

--- entrega-01/ProductManager.js
+++ entrega-01/ProductManager.js
@@ -60,7 +60,7 @@ console.log("// Definir un nuevo producto")
 productManager.addProduct(new Product( "Producto prueba 0", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25));
 // Variante 1
 productManager.addProduct(new Product( "Producto Prueba 1", "Este es el primer producto de prueba", 100, "Sin imagen", "def456", 30));
-// Variante 2
+// Variante 2 
 productManager.addProduct(new Product( "Producto Prueba 2", "Este es el segundo producto de prueba", 300, "Sin imagen", "ghi789", 35));
 // Variante 3
 productManager.addProduct(new Product( "Producto Prueba 3", "Este es el tercer producto de prueba", 400, "Sin imagen", "jkl012", 40));
