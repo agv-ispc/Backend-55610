af72203a85819a6c0d473c2366c500515d1e2c0a
Adrian Gimenez <106855460+agv-ispc@users.noreply.github.com>
2024-01-24 10:27:41 -0300

    Update vistas

--- entrega-05/src/routes/views.router.js
+++ entrega-05/src/routes/views.router.js
@@ -1,10 +1,16 @@
-import { Router } from "express";
+import { Router, application } from "express";
+import cookieParser from "cookie-parser";
 import productsDao from '../daos/db/products.dao.js';
 import cartsDao from "../daos/db/carts.dao.js";
+import dotenv from 'dotenv';
 
-const viewsRouter = Router();
+dotenv.config();
 
-viewsRouter.get("/", async (req, res) => {
+const router = Router();
+
+router.use(cookieParser(process.env.COOKIEPASS))
+
+router.get("/", async (req, res) => {
   const products = await productsDao.getAllProducts();
   try {
     res.render("index", { products: products });
@@ -13,7 +19,7 @@ viewsRouter.get("/", async (req, res) => {
   }
 });
 
-viewsRouter.get("/products", async (req, res) => {
+router.get("/products", async (req, res) => {
   try {
     const products = await productsDao.getAllProducts();
     res.json(products);
@@ -22,7 +28,7 @@ viewsRouter.get("/products", async (req, res) => {
   }
 });
 
-viewsRouter.get("/carts", async (req, res) => {
+router.get("/carts", async (req, res) => {
   try {
     const carts = await cartsDao.getAllCarts();
     res.json(carts);
@@ -31,7 +37,7 @@ viewsRouter.get("/carts", async (req, res) => {
   }
 });
 
-viewsRouter.get("/carts/:cid", async (req, res) => {
+router.get("/carts/:cid", async (req, res) => {
   try {
     const cart = await cartsDao.getCartById(cid);
     res.json(cart);
@@ -40,14 +46,43 @@ viewsRouter.get("/carts/:cid", async (req, res) => {
   }
 });
 
-viewsRouter.get("/realtimeproducts", async (req, res) => {
+router.get("/realtimeproducts", async (req, res) => {
   const products = await productsDao.getAllProducts();
   res.render("realTimeProducts", { products })
 })
 
-viewsRouter.get("/realtimecarts", async (req, res) => {
+router.get("/realtimecarts", async (req, res) => {
   const carts = await cartsDao.getAllCarts();
   res.render("realTimeCarts", { carts })
 })
 
-export default viewsRouter;
+router.get('/session', (req, res) => {
+  if(req.session.counter){
+    req.session.counter++;
+    res.send(`Sesion número ${req.session.counter}`)
+  } else {
+    req.session.counter = 1;
+    res.send('Bienvenido')
+  }
+})
+
+router.get('/setcookie', (req, res) => {
+  res.cookie('SessionCookie', 'Cookie firmada!!', { maxAge: 30000, signed: true}).send('Cookie asignada con exito')
+})
+
+router.get('/getcookie', (req, res) => {
+  res.send(req.signedCookies)
+})
+
+router.get('/deletecookie', (req, res) => {
+  res.clearCookie('SessionCookie').send('Cookie borrada!!')
+})
+
+router.get('/logout', (req, res) => {
+  req.session.destroy( error => {
+    if(!error) res.send('Logout ok!')
+    else res.send({status: 'Logout ERROR', body: error})
+  })
+})
+
+export default router;
--- entrega-06/src/server.js
+++ entrega-06/src/server.js
@@ -1,19 +1,39 @@
 // Importamos los módulos necesarios
 import express from 'express';
 import mongoose from 'mongoose';
-import { __dirname } from './dirname.js';
-import path from 'path';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
 import handlebars from 'express-handlebars';
 import { Server } from 'socket.io';
 import Handlebars from 'handlebars';
 import { allowInsecurePrototypeAccess } from '@handlebars/allow-prototype-access';
 import dotenv from 'dotenv';
-import session from 'express-session';
+import passport from 'passport';
+import authPassport from './auth/auth.js';
+import path from 'path';
+
+import { __dirname } from './dirname.js';
 
 // Creamos una instancia de la aplicación Express
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Configuramos la conexión a la base de datos MongoDB
+app.engine('hbs', handlebars.engine({
+  extname: ".hbs",
+  defaultLayout: "main",
+  partialsDir: `${__dirname}/views`,
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
+}));
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'hbs');
+app.use(express.static(`${__dirname}/public`))
+
+// Configuramos el middleware para manejar solicitudes no encontradas
+app.use((req, res) => {
+  res.status(404).send("Error - pagina no encontrada");
+});
+
 dotenv.config();
 
 const cookiepass = process.env.COOKIEPASS;
@@ -22,66 +42,29 @@ const password = process.env.DB_PASSWORD;
 const cluster = process.env.DB_CLUSTER;
 const MongoUrl = `mongodb+srv://${user}:${password}@${cluster}`
 
-
 mongoose.set('strictQuery', true);
 
 // Intentamos conectar a la base de datos y manejamos posibles errores
 mongoose
   .connect(MongoUrl)
-  //.connect('mongodb+srv://agvispc:Neil.96864@cluster0.modbxsw.mongodb.net/ecommerce')
   .then(() => console.log("DB conected"))
   .catch((err) => console.log(err));
 
-// Configuramos el middleware para analizar solicitudes JSON
-app.use(express.json());
-
-// Configuramos el middleware para parsear los datos en las solicitudes POST con formato 'application/x-www-form-urlencoded'
-app.use(express.urlencoded({ extended: true }))
-
-// Importamos las rutas de productos
-import viewsRouter from './routes/views.router.js';
-
-// Configuramos el motor de plantillas Handlebars
-app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: ".hbs",
-    defaultLayout: "main",
-    handlebars: allowInsecurePrototypeAccess(Handlebars),
-  })
-);
-
-// Configuramos el motor de vistas y el directorio de vistas
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, '/views'));
-app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
+  store: MongoStore.create({
+    mongoUrl: MongoUrl,
+    mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+    ttl: 60
+  }),
   secret: cookiepass,
-
-  resave: true,
+  resave: false,
   saveUninitialized: true
-}))
+}));
 
-// Usamos las rutas de productos
-app.use("/", viewsRouter);
+authPassport();
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Configuramos la conexión a la base de datos
-const db = mongoose.connection;
-
-// Manejamos los errores de conexión a la base de datos
-db.on('error', (error) => {
-  console.error(`Error de conexion a la base de datos: ${error.message}`);
-})
-
-// Confirmamos la conexión exitosa a la base de datos
-db.once('open', () => {
-  console.log('Conexion exitosa a la base de datos');
-})
-
-// Configuramos el middleware para manejar solicitudes no encontradas
-app.use((req, res) => {
-  res.status(404).send("Error - pagina no encontrada");
-});
 
 // Definimos el puerto en el que el servidor escuchará las solicitudes
 const PORT = process.env.PORT;
@@ -90,7 +73,6 @@ const PORT = process.env.PORT;
 const httpServer = app.listen(PORT, () =>
   console.log(`Server listening on port ${PORT}`)
 );
-
 // Configuramos Socket.IO para manejar conexiones de clientes
 const io = new Server(httpServer, {
   connectionStateRecovery: {}
@@ -106,10 +88,7 @@ io.on('connection', (socket) => {
   socket.on('carts-list', (carts) => {
     io.emit('carts-list', carts)
   })
-
   socket.on('products-list', (products) => {
     io.emit('products-list', products)
   })
-
-
 })
